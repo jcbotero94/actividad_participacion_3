7a1c299cd266ef11f0336343dda7bbac9749c133
Juan Camilo Botero <juancabotero94@gmail.com>
2023-08-12 11:16:49 -0500

    mi segundo commit

---
+++ ejercicio_participacion_3/n1.py
@@ -0,0 +1,6 @@
+class vehiculo:
+    def __init__(self):
+        self. velocidad_maxima=80
+        self. kilometraje=0
+        
+        
---
+++ ejercicio_participacion_3/n2.py
@@ -0,0 +1,10 @@
+class punto:
+    def __init__(self,coord_x,coord_y):
+        self. coord_x=coord_x
+        self. coord_y=coord_y
+        
+    def coordenadas(self):
+        print (f"(self.coord_x),(self.coord_y)")
+    def coordenadas(self,mover_x,mover_y):
+        self.coord_x=mover_x  
+        self.coord_y=mover_y 
